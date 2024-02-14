56cb0aa46ae4adf34c3db198249bd104cb674544
lombrinoArnolfo <159927437+lombrinoArnolfo@users.noreply.github.com>
2024-02-14 20:11:37 +0800

    Add files via upload

---
+++ ARNOLFO_ACTIVITY11.java
@@ -0,0 +1,35 @@
+
+package arnolfo_activity11;
+
+import java.util.Scanner;
+
+
+public class ARNOLFO_ACTIVITY11 {
+    
+        static Scanner InputNum = new Scanner (System.in);
+    static Scanner InputString= new Scanner (System.in);
+    
+    public static void main(String[] args) {
+       
+
+    
+    System.out.print("Name: ");
+    String name = InputString.nextLine();
+    System.out.print("Age: ");
+    int age = InputNum.nextInt();
+    System.out.print("Gender: ");
+    String gender = InputString.nextLine();
+    System.out.print("Address: ");
+    String address = InputString.nextLine();
+    System.out.print("Civil Status: ");
+    String Civil_Status = InputString.nextLine();
+    System.out.print("# of Brothers/Sisters: ");
+    int Brothers_And_Sisters = InputNum.nextInt();
+
+
+    }
+
+}
+    
+    
+
