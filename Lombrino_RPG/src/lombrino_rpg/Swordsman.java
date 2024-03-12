dc2f001b040910164e009eee4f3b187f7f501156
lombrinoArnolfo <159927437+lombrinoArnolfo@users.noreply.github.com>
2024-03-12 16:05:02 +0800

    lab_act2.2

---
+++ Lombrino_RPG/src/lombrino_rpg/Swordsman.java
@@ -0,0 +1,11 @@
+
+package lombrino_rpg;
+
+
+public class Swordsman extends Character {
+       public Swordsman (int health, int damage, int shield){
+        super (health, damage, shield);
+    } 
+    
+    
+}
